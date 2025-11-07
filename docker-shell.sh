05a6a5838147efca234e645c43a03aa6090ef92b
Kaylee Vo <kayleeyvo@gmail.com>
2025-11-07 21:58:13 +0000

    dataset updates...

--- docker-shell.sh
+++ docker-shell.sh
@@ -14,14 +14,34 @@ echo "Building image"
 docker build -t data-version-cli -f Dockerfile .
 
 echo "Running container"
+# docker run --rm --name data-version-cli -ti \
+# --privileged \
+# --cap-add SYS_ADMIN \
+# --device /dev/fuse \
+# -v "$BASE_DIR":/app \
+# -v "$SECRETS_DIR":/secrets \
+# -v ~/.gitconfig:/etc/gitconfig \
+# -e GOOGLE_APPLICATION_CREDENTIALS=$GOOGLE_APPLICATION_CREDENTIALS \
+# -e GCP_PROJECT=$GCP_PROJECT \
+# -e GCP_ZONE=$GCP_ZONE \
+# -e GCS_BUCKET_NAME=$GCS_BUCKET_NAME data-version-cli
+
+# docker run --rm --name data-version-cli -ti \
+# --privileged \
+# --cap-add SYS_ADMIN \
+# --device /dev/fuse \
+# -v "$BASE_DIR":/app \
+# -v "$SECRETS_DIR":/secrets \
+# -v ~/.gitconfig:/etc/gitconfig \
+# -e GOOGLE_APPLICATION_CREDENTIALS=$GOOGLE_APPLICATION_CREDENTIALS \
+# -e GCS_BUCKET_NAME=$GCS_BUCKET_NAME data-version-cli
+
 docker run --rm --name data-version-cli -ti \
 --privileged \
---cap-add SYS_ADMIN \
---device /dev/fuse \
 -v "$BASE_DIR":/app \
 -v "$SECRETS_DIR":/secrets \
 -v ~/.gitconfig:/etc/gitconfig \
 -e GOOGLE_APPLICATION_CREDENTIALS=$GOOGLE_APPLICATION_CREDENTIALS \
--e GCP_PROJECT=$GCP_PROJECT \
--e GCP_ZONE=$GCP_ZONE \
 -e GCS_BUCKET_NAME=$GCS_BUCKET_NAME data-version-cli
+
+dvc remote add -d cheese_dataset gs://kaylee-ac215-hw2/dvc_store
